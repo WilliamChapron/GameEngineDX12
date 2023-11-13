1e74bc0983e39c8a1d85541b4da1b04aeb36c33f
wchapron <wchapron@GCFRLY248>
2023-11-13 12:21:31 +0100

    dsd

--- GameEngineDX12/main.cpp
+++ GameEngineDX12/main.cpp
@@ -33,6 +33,15 @@
 #include "EngineManager.h"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+    // Crée une console pour l'affichage de la sortie
+    AllocConsole();
+
+    FILE* pConsole;
+    if (freopen_s(&pConsole, "CONOUT$", "w", stdout) != 0) {
+        MessageBox(0, L"Failed to redirect console output", L"Error", MB_OK);
+        return 1;
+    }
+    std::cout << "success" << std::endl;
 
 
     DirectInitializer* direct3dInstance = new DirectInitializer();
@@ -42,19 +51,18 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     EngineManager* engineInstance = new EngineManager(&hInstance);
 
-
     // initialize direct3d
     if (!direct3dInstance)
     {
-        MessageBox(0, L"Failed to initialize direct3d 12",L"Error", MB_OK);
+        MessageBox(0, L"Failed to initialize direct3d 12", L"Error", MB_OK);
         direct3dInstance->Cleanup();
         return 1;
     }
 
-    bool success = direct3dInstance->CreateDXGIFactory();
-    std::cout << success << std::endl;
+    
 
-
+    //bool success = direct3dInstance->CreateDXGIFactory();
+    //std::cout << success << std::endl;
 
     //// we want to wait for the gpu to finish executing the command list before we start releasing everything
     //direct3dInstance->WaitForPreviousFrame();
@@ -62,7 +70,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     //// close the fence event
     //direct3dInstance->CloseHandle(fenceEvent);
 
-
-
+    // Ferme la console avant la fin du programme
+    fclose(pConsole);
+    FreeConsole();
     return 0;
 }
